0666704cc0070a03e36486d8c0f4f2c800bf9438
Alex Birchfield <abirchfield@acumensolutions.com>
2019-03-20 13:14:56 -0400

    script update

--- myscript.sh
+++ myscript.sh
@@ -5,31 +5,32 @@
 # cp -r $x .
 # echo "$x copied"
 # done
+if [ -d /classes ]
+then
+  RELEASECLASSES=(classes/*)
+  RELEASEFOLDER=~/Documents/stars/classes
+  xml="meta.xml"
+  cd ..
+  cd tools
+  MASTERCLASSES=(classes/*)
 
-RELEASECLASSES=(classes/*)
-
-RELEASEFOLDER=~/Documents/stars/classes
-xml="meta.xml"
-cd ..
-cd tools
-MASTERCLASSES=(classes/*)
-
-for x in "${RELEASECLASSES[@]}" 
-do
-echo ---"$x"---
-  for y in "${MASTERCLASSES[@]}"
+  for x in "${RELEASECLASSES[@]}" 
   do
-  echo ---"$y"---
-    if [ "$x-$xml" == "$y" ]
-      then
-        echo "$x-$xml and $y MATCH!"
-        cp "$y" $RELEASEFOLDER
-    fi
+  echo ---"$x"---
+    for y in "${MASTERCLASSES[@]}"
+    do
+    echo ---"$y"---
+      if [ "$x-$xml" == "$y" ]
+        then
+          echo "$x-$xml and $y MATCH!"
+          cp "$y" $RELEASEFOLDER
+      fi
+    done
   done
-done
 
-cd ..
-cd stars
-git add .
-git commit -m "class meta-xml files"
-git push
+  cd ..
+  cd stars
+  git add .
+  git commit -m "class meta-xml files"
+  git push
+fi
